ec6bfa1a0fd6883cda60ed5df96e202d5e71bbf7
Vlad Frangu <kingdgrizzle@gmail.com>
2022-02-01 23:06:00 +0200

    chore: it just #works

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { MikroORM } from '@mikro-orm/core';
 import type { SqliteDriver } from '@mikro-orm/sqlite';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { Hug } from './entities/Hug.js';
 
 console.log({ imports: process.env.MIKRO_ORM_DYNAMIC_IMPORTS });
 
 const orm = await MikroORM.init<SqliteDriver>({
-	entities: ['./entities'],
+	entities: ['./dist/entities'],
 	dbName: 'owo.sqlite',
 	type: 'sqlite',
-	baseDir: dirname(fileURLToPath(import.meta.url)),
+	// baseDir: dirname(fileURLToPath(import.meta.url)),
 });
 
 await orm.getSchemaGenerator().ensureDatabase();
@@ -18,11 +16,12 @@ await orm.getSchemaGenerator().updateSchema({
 	dropTables: true,
 });
 
-console.log(orm.em);
-
 const hugOne = new Hug();
 const hugTwo = new Hug();
 
-console.log(await orm.em.fork().persistAndFlush([hugOne, hugTwo]));
+await orm.em.fork().persistAndFlush([hugOne, hugTwo]);
+const entities = await orm.em.fork().find(Hug, {});
+console.log(entities);
 
+await orm.em.fork().removeAndFlush(entities);
 await orm.close();
