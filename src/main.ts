e30377383baa378f477be2f40297108271fe661e
Vlad Frangu <kingdgrizzle@gmail.com>
2022-02-01 22:49:34 +0200

    chore: fin/

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { Hug } from './entities/Hug.js';
 
+console.log({ imports: process.env.MIKRO_ORM_DYNAMIC_IMPORTS });
+
 const orm = await MikroORM.init<SqliteDriver>({
 	entities: ['./entities'],
-	dbName: 'owo',
+	dbName: 'owo.sqlite',
 	type: 'sqlite',
 	baseDir: dirname(fileURLToPath(import.meta.url)),
+});
+
+await orm.getSchemaGenerator().ensureDatabase();
+await orm.getSchemaGenerator().updateSchema({
+	dropTables: true,
 });
 
 console.log(orm.em);
@@ -16,4 +23,6 @@ console.log(orm.em);
 const hugOne = new Hug();
 const hugTwo = new Hug();
 
-console.log(await orm.em.persistAndFlush([hugOne, hugTwo]));
+console.log(await orm.em.fork().persistAndFlush([hugOne, hugTwo]));
+
+await orm.close();
